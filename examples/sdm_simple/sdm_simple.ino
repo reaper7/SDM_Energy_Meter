e947b2d8da1f07f2e6ea539cbf9ac601929a5b9c
reaper7 <reaper7md@gmail.com>
2017-05-07 12:39:57 +0200

    Update sdm_simple.ino

--- examples/sdm_simple/sdm_simple.ino
+++ examples/sdm_simple/sdm_simple.ino
@@ -4,15 +4,15 @@
 
 char bufout[10];
 
-//SDM<2400, 12, 13> sdm;                                                        //SDM120T	baud, rx pin, tx pin
-//SDM<4800, 12, 13> sdm;                                                        //SDM220T	baud, rx pin, tx pin
-//SDM<9600, 12, 13> sdm;                                                        //SDM630	baud, rx pin, tx pin
+//SDM<2400, 12, 13> sdm;                                                        //SDM120T	baud, rx pin, tx pin, dere pin(optional for max485)
+//SDM<4800, 12, 13> sdm;                                                        //SDM220T	baud, rx pin, tx pin, dere pin(optional for max485)
+//SDM<9600, 12, 13> sdm;                                                        //SDM630	baud, rx pin, tx pin, dere pin(optional for max485)
 //or without parameters (default from SDM.h will be used): 
 SDM<> sdm;
 
 void setup() {
   Serial.begin(115200);                                                         //initialize serial
-  sdm.begin();                                                                  //initalize SDM220 communication baudrate
+  sdm.begin();                                                                  //initialize SDM220 communication baudrate
 }
 
 void loop() {
