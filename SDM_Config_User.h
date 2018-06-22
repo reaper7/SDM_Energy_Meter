a884bc62a110a488e37cc59a8718115898e9ceaf
reaper7 <reaper7md@gmail.com>
2018-06-22 10:04:35 +0200

    update user config

--- SDM_Config_User.h
+++ SDM_Config_User.h
@@ -5,21 +5,28 @@
 */
 //USER CONFIG:
 //------------------------------------------------------------------------------
+/*
 #ifdef USE_HARDWARESERIAL
   #undef USE_HARDWARESERIAL                                                     //undefine USE_HARDWARESERIAL from SDM_Config.h
 #endif
 #define USE_HARDWARESERIAL                                                      //define USE_HARDWARESERIAL
+*/
 //------------------------------------------------------------------------------
+/*
 #ifdef SDM_UART_BAUD
   #undef SDM_UART_BAUD                                                          //undefine baudrate from SDM_Config.h
 #endif
 #define SDM_UART_BAUD                       9600                                //define new baudrate
+*/
 //------------------------------------------------------------------------------
+/*
 #ifdef DERE_PIN
   #undef DERE_PIN                                                               //undefine DERE_PIN from SDM_Config.h
 #endif
 #define DERE_PIN                            NOT_A_PIN                           //define new DERE_PIN for control MAX485 DE/RE lines (connect DE & /RE together to this pin)
+*/
 //------------------------------------------------------------------------------
+/*
 #ifdef USE_HARDWARESERIAL
 
   #ifdef SDM_UART_CONFIG
@@ -33,9 +40,12 @@
   #define SWAPHWSERIAL                      0                                   //define new SWAPHWSERIAL, if true(1) then swap uart pins from 3/1 to 13/15 (only ESP8266)
 
 #endif
+*/
 //------------------------------------------------------------------------------
+/*
 #ifdef MAX_MILLIS_TO_WAIT
   #undef MAX_MILLIS_TO_WAIT                                                     //undefine MAX_MILLIS_TO_WAIT from SDM_Config.h
 #endif
 #define MAX_MILLIS_TO_WAIT                  500                                 //define new MAX_MILLIS_TO_WAIT to wait for response from SDM
+*/
 //------------------------------------------------------------------------------
