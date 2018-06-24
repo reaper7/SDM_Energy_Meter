c5bbf4ec51fcda522efe7088dc880b43cd46a0d6
reaper7 <reaper7md@gmail.com>
2018-06-24 12:10:20 +0200

    update user config

--- SDM_Config_User.h
+++ SDM_Config_User.h
@@ -6,9 +6,7 @@
 //USER CONFIG:
 //------------------------------------------------------------------------------
 /*
-#ifdef USE_HARDWARESERIAL
-  #undef USE_HARDWARESERIAL                                                     //undefine USE_HARDWARESERIAL from SDM_Config.h
-#endif
+#undef USE_HARDWARESERIAL                                                       //undefine USE_HARDWARESERIAL
 */
 // or
 /*
@@ -16,39 +14,23 @@
 */
 //------------------------------------------------------------------------------
 /*
-#ifdef SDM_UART_BAUD
-  #undef SDM_UART_BAUD                                                          //undefine baudrate from SDM_Config.h
-#endif
-#define SDM_UART_BAUD                       9600                                //define new baudrate
+#define SDM_UART_BAUD                       9600                                //define user baudrate
 */
 //------------------------------------------------------------------------------
 /*
-#ifdef DERE_PIN
-  #undef DERE_PIN                                                               //undefine DERE_PIN from SDM_Config.h
-#endif
-#define DERE_PIN                            NOT_A_PIN                           //define new DERE_PIN for control MAX485 DE/RE lines (connect DE & /RE together to this pin)
+#define DERE_PIN                            NOT_A_PIN                           //define user DERE_PIN for control MAX485 DE/RE lines (connect DE & /RE together to this pin)
 */
 //------------------------------------------------------------------------------
-/*
 #ifdef USE_HARDWARESERIAL
-
-  #ifdef SDM_UART_CONFIG
-    #undef SDM_UART_CONFIG                                                      //undefine SDM_UART_CONFIG from SDM_Config.h
-  #endif
-  #define SDM_UART_CONFIG                   SERIAL_8N1                          //define new SDM_UART_CONFIG
-
-  #ifdef SWAPHWSERIAL
-    #undef SWAPHWSERIAL                                                         //undefine SWAPHWSERIALG from SDM_Config.h
-  #endif
-  #define SWAPHWSERIAL                      0                                   //define new SWAPHWSERIAL, if true(1) then swap uart pins from 3/1 to 13/15 (only ESP8266)
-
-#endif
+/*
+  #define SDM_UART_CONFIG                   SERIAL_8N1                          //define user SDM_UART_CONFIG
 */
+/*
+  #define SWAPHWSERIAL                      0                                   //define user SWAPHWSERIAL, if true(1) then swap uart pins from 3/1 to 13/15 (only ESP8266)
+*/
+#endif
 //------------------------------------------------------------------------------
 /*
-#ifdef MAX_MILLIS_TO_WAIT
-  #undef MAX_MILLIS_TO_WAIT                                                     //undefine MAX_MILLIS_TO_WAIT from SDM_Config.h
-#endif
-#define MAX_MILLIS_TO_WAIT                  500                                 //define new MAX_MILLIS_TO_WAIT to wait for response from SDM
+#define MAX_MILLIS_TO_WAIT                  500                                 //define user MAX_MILLIS_TO_WAIT to wait for response from SDM
 */
 //------------------------------------------------------------------------------
